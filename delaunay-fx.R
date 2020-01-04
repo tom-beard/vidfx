e085d43d0aa762adbe184068bff12ed6d4391499
Tom <wellurban@gmail.com>
2020-01-04 18:37:54 +1300

    Explore Hessian determinants at one level

--- delaunay-fx.R
+++ delaunay-fx.R
@@ -17,8 +17,29 @@ input_vid <- load.video(path(vid_src_dir, vid_filename), fps = input_fps, maxSiz
 sample_frames <- imsplit(input_vid, "z")
 # sample_frames[1:12] %>% plot()
 sample_frame <- sample_frames[[7]] %>% grayscale() %>% resize_halfXY()
-
+sample_frame %>% plot()
 
 # blob detection ----------------------------------------------------------
 
-# adapted from https://dahtah.github.io/imager/gettingstarted.html
+# adapted from examples at https://dahtah.github.io/imager/gettingstarted.html
+
+Hdet <- with(imhessian(sample_frame), (xx * yy - xy^2))
+Hdet %>% plot(main = "Determinant of Hessian")
+
+this_thresh <- 0.001
+thresh_text <- glue("{100 * (1 - this_thresh)}%")
+threshold(Hdet, thresh_text) %>%
+  plot(main = glue("Determinant: {100 * this_thresh}% highest values"))
+
+lab <- threshold(Hdet, thresh_text) %>% label()
+plot(lab)
+
+df <- lab %>% as.data.frame() %>% as_tibble() %>% filter(value > 0)
+# unique(df$value)
+centres <- df %>% 
+  group_by(value) %>% 
+  summarise(mx = mean(x), my = mean(y))
+plot(sample_frame)
+with(centres, points(mx, my, col = "red"))
+
+
