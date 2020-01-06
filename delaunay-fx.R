a89def73a8499186729b58a2ce23bed45af113e8
Tom <wellurban@gmail.com>
2020-01-06 21:05:00 +1300

    Add documentation for get_centres()

--- delaunay-fx.R
+++ delaunay-fx.R
@@ -23,12 +23,24 @@ sample_frame %>% plot()
 
 # adapted from examples at https://dahtah.github.io/imager/gettingstarted.html
 
+#' Find the coordinates of each blob in the image.
+#'
+#' @param im A grayscale image of type cimg
+#' @param this_thresh Quantile for the threshold stage (default 0.01 selects the top 1%)
+#' @param sigma Standard deviation of the blur to apply before blob detection
+#'
+#' @return A tibble with a row for each blob, and columns named `value` (the numerical label for the blob),
+#'   `mx` (x value for the centre of the blob) and `my` (y value for the centre of the blob).
+#'
+#' @examples
+#' imname <- system.file('extdata/parrots.png', package = 'imager')
+#' points <- load.image(imname) %>% grayscale %>% get_centres(sigma = 2)
 get_centres <- function(im, this_thresh = 0.01, sigma = 0) {
   im %>% 
     isoblur(sigma) %>% 
     imhessian() %$%
     { xx * yy - xy^2 } %>%
-    threshold(glue("{100 * (1 - this_thresh)}%")) %>%
+    threshold(glue::glue("{100 * (1 - this_thresh)}%")) %>%
     label() %>%
     as.data.frame() %>%
     as_tibble() %>%
