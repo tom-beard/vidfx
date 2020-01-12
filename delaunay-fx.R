ed67648317ef3221a8d83a101c01dd22ea96e7b5
Tom <wellurban@gmail.com>
2020-01-12 19:15:17 +1300

    Add multiscale blob detection for Delaunay

--- delaunay-fx.R
+++ delaunay-fx.R
@@ -33,7 +33,7 @@ hessdet <- function(im, scale = 1) {
 #' Find the coordinates of each blob in the image.
 #'
 #' @param im A grayscale image of type cimg
-#' @param this_thresh Quantile for the threshold stage (default 0.01 selects the top 1%)
+#' @param threshold Quantile for the threshold stage (default 0.01 selects the top 1%)
 #' @param scale Standard deviation of the blur to apply before blob detection
 #'
 #' @return A tibble with a row for each blob, and columns named `value` (the numerical label for the blob),
@@ -66,22 +66,46 @@ with(get_centres(sample_frame, 0.001, scale = 4), points(mx, my, col = "red"))
 
 # multi-level blob detection ----------------------------------------------
 
-max_hessdet <- function(im,
+find_multiscale_blobs <- function(im,
                         scales = seq(from = max(dim(im)[1:2]) %/% 200,
                                      to = max(dim(im)[1:2]) %/% 8,
                                      by = 1)) {
-  scales %>%
-    map(~ hessdet(im, .)) %>%
-    which.parmax()
+  multiscale_hessdet <- scales %>% map(~ hessdet(im, .))
+  list(i_max = which.parmax(multiscale_hessdet), d_max = parmax(multiscale_hessdet))
 }
 
-i_max <- sample_frame %>% max_hessdet(seq(12, 100, by = 1))
-i_max %>% plot(main = "Index of point-wise max\nacross scales")
+blobs <- sample_frame %>% find_multiscale_blobs(seq(12, 100, by = 1))
+blobs$i_max %>% plot(main = "Index of point-wise max\nacross scales")
+blobs$d_max %>% plot()
+
+#' Find the coordinates of each blob in the image over multiple scale.
+#'
+#' @param im A grayscale image of type cimg
+#' @param threshold Quantile for the threshold stage (default 0.01 selects the top 1%)
+#' @param scales Standard deviation of the blur to apply before blob detection
+#'
+#' @return A tibble with a row for each blob, and columns named `scale_index` (the scale of the blob),
+#'   `mx` (x value for the centre of the blob) and `my` (y value for the centre of the blob).
+#'
+#' @examples
+#' imname <- system.file('extdata/parrots.png', package = 'imager')
+#' points <- load.image(imname) %>% grayscale %>% get_multiscale_scales(scales = 4:10)
+get_multiscale_blobs <- function(im, threshold = 0.01, scales) {
+  blobs <- im %>% find_multiscale_blobs(seq(12, 100, by = 1))
+  blobs$d_max %>% 
+    threshold(glue::glue("{100 * (1 - threshold)}%")) %>%
+    label() %>%
+    as.data.frame() %>%
+    as_tibble() %>%
+    mutate(index = as.data.frame(blobs$i_max)$value) %>% 
+    # filter(value > 0) %>% 
+    group_by(value) %>% 
+    summarise(mx = mean(x), my = mean(y), scale_index = mean(index))
+}
 
 
 # add Delaunay triangulation ----------------------------------------------
 
-centres_df <- get_centres(sample_frame, threshold = 0.001, scale = 4)
 
 # a wrapper so that deldir() takes a tibble as an argument
 # next steps: add options to return one of the main objects as a tibble
@@ -89,6 +113,7 @@ deldir_tidy <- function(df) {
   deldir::deldir(df$mx, df$my)
 }
 
+centres_df <- get_centres(sample_frame, threshold = 0.001, scale = 4)
 delaunay <- centres_df %>% deldir_tidy()
 
 sample_frame %>%
@@ -103,3 +128,17 @@ sample_frame %>%
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_continuous(expand = c(0, 0), trans = scales::reverse_trans())
 
+centres_multiscale_df <- get_multiscale_blobs(sample_frame, threshold = 0.04, scales = seq(12, 100, by = 1))
+delaunay_ms <- centres_multiscale_df %>% deldir_tidy()
+
+sample_frame %>%
+  as.data.frame() %>% 
+  ggplot(aes(x, y)) +
+  geom_raster(aes(fill = value)) +
+  geom_segment(data = delaunay_ms$delsgs, aes(x = x1, y = y1, xend = x2, yend = y2),
+               colour = "yellow", alpha = 0.5) +
+  geom_point(data = centres_multiscale_df,
+             aes(mx, my), colour = "red") +
+  scale_fill_gradient(low = "black", high = "white") +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0), trans = scales::reverse_trans())
